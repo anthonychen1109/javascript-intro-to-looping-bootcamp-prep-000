f9e2ca8e7d7021d82143fd03f81326c7e28169a4
Anthony Chen <chen.anthony.1109@gmail.com>
2018-06-14 01:41:05 +0000

    automatically backed up by learn

---
+++ loops.js
@@ -0,0 +1,19 @@
+function forLoop(arr) {
+  for (let i=0; i<25; i++) {
+    arr.push(`I am ${i} strange loop${i === 1 ? '' : 's'}.`) 
+  }
+  return arr
+}
+
+function whileLoop(n) {
+  while(n >= 0) {
+    console.log(n)
+    n -= 1
+  }
+}
+
+function doWhileLoop(arr) {
+  do {
+    arr.pop()
+  } while(arr.length >= 0 || !maybeTrue())
+}
